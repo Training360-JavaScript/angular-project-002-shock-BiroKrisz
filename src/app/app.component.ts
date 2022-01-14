bc457655390cf5efdca34769e2ae446b2dc554ed
BiroKrisz <biro.s.kr@gmail.com>
2022-01-14 17:13:43 +0100

    Almost done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,27 @@ import { UserService } from './service/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ UserService ]
 })
 export class AppComponent {
   title = 'The good Angular programmer';
 
-  constructor() {}
+  userData!: User[]
+  currentUser: User = new User
 
+  constructor(private userService: UserService) {
+    this.userData = this.userService.list
+  }
+
+  onSelectClick(currentUser: User): void {
+    this.currentUser = currentUser
+  }
+  onUpdateClick(currentUser: User): void {
+    this.userService.updateUser(currentUser)
+  }
+  onDeleteClick(currentUser: User): void {
+    this.userService.removeUser(currentUser)
+  }
 }
+
